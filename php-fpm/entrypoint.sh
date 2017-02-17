92475bda0948d3ebd89899bf0866a6a5547158cd
Alexander Trost <galexrt@googlemail.com>
2017-02-18 00:05:47 +0100

    Use the php:7-fpm-alpine as the base image

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# TODO
+if [ -z "$REDIS_ADDRESS" ]; then
+    echo "REDIS_ADDRESS is empty. At least one Redis server address needs to be given."
+    exit 1
+fi
 
-exec php-fpm
+sed -i 's/^(;)session.save_path.*/session.save_path = '"$REDIS_ADDRESS"'/' /etc/php/fpm
+
+exec php-fpm -F
